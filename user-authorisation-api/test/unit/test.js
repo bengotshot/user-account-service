478ca0c0a72b9e20c16091d679962def14bc8de7
Ben Wallman <bwallman@andigital.com>
2017-05-31 17:52:44 +0100

    Updated to include test running and test watchers. TDD on all future stuff, probably. Probably not actually, but hopefully.

---
+++ user-authorisation-api/test/unit/test.js
@@ -0,0 +1,8 @@
+import { assert, expect } from 'chai';
+import { describe, it } from 'mocha';
+
+describe('tests', () => {
+    it('should have at least one test', ()=> {
+        expect(1+1).to.equal(2);
+    });
+});
---
+++ user-management-api/test/unit/test.js
@@ -0,0 +1,7 @@
+import { assert, expect } from 'chai';
+import { describe, it } from 'mocha';
+
+describe('tests', () => {
+    expect(1+1).to.equal(2);
+    console.log("well done");
+});
